512d12e75f6926500d4326a5f07b946d0c520138
Glenn Key <glennkey@Glenns-MacBook-Pro.local>
2019-09-22 22:12:33 +0200

    finessed my final product, added html features

--- javascript1_ma2.js
+++ javascript1_ma2.js
@@ -20,36 +20,37 @@ myArrayOfNumbers.pop();
 // strawberry to banana and strawberries to bananas: 
 
   function wordChanger() {
-    var str = 'Strawberries are a popular part of spring and summer diets throughout America. Mouths water from coast to coast each spring, when small white blossoms start to appear on strawberry bushes. They announce the impending arrival of the ruby red berries that so many people crave. Ripe strawberries taste sweet and have only a slight hint of tartness. They are also one of the healthiest fruits around. There are countless recipes for the luscious red berry, but many people prefer to eat them fresh and unaccompanied.';
-    var res1 = str.replace(/Strawberries/g, "Bananas");
-    var res2 = res1.replace(/strawberry/gi, "banana");
-    var res3 = res2.replace(/strawberries/g, "bananas");
-        return res3;
+    var str = document.getElementById("string").innerHTML;
+    var editedString = str.replace(/Strawberries/g, "Bananas").replace(/strawberry/gi, "banana").replace(/strawberries/g, "bananas");
+    
+    document.getElementById("string").innerHTML = editedString;
   };
 
-  console.log(wordChanger());
+  wordChanger();
 
 
 //5. Create an array and a button. The array should contain 4 football clubs names. When the button is clicked, 
 // remove all elements out of the array that we just created. There after add in 4 names of cars inside that array. Log it to the console.
 
-var dynamicArray = ['Juventus', 'TIL', 'Manchester UTD', 'Liverpool'];
+var dynamicArray = ['Juventus', ' TIL', ' Manchester UTD', ' Liverpool'];
 
 console.log(dynamicArray);
-document.write(dynamicArray);
+
+document.getElementById("array").innerHTML = dynamicArray;
 
 document.getElementById("btn").onclick = function() {arraySwitcher()}; 
 
 function arraySwitcher(){
 
-    dynamicArray.splice(0, 4, 'Ferrari', 'Mercedes', 'Porsche', 'BMW');
+    dynamicArray.splice(0, 4, 'Ferrari', ' Mercedes', ' Porsche', ' BMW');
 
     console.log(dynamicArray);
-    return document.write(dynamicArray);
+    return document.getElementById("array").innerHTML = dynamicArray;
 };
 
 //6. Create an array of objects with 3 people inside it. Use the filter function to map by a name.
- var arrayOfPeople = [
+
+var arrayOfPeople = [
 
     {   name : 'Jim',
         age : 20,
@@ -67,14 +68,9 @@ function arraySwitcher(){
     }
     ];
 
- var people = arrayOfPeople.filter(function(name) {
-     return name.name === 'Jake';
- });
+ var jake = arrayOfPeople.filter(person => person.name === 'Jake');
 
 //7. Create a simple function that logs the date.
 
 var date = new Date();
 console.log(date);
-
-
-
